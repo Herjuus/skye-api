dc26da014acd624aab63ddbf132e66e6235a77be
Herjus <herjus420@gmail.com>
2023-09-17 12:55:13 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,25 +1,9 @@
 import SkyeAPI from "./lib/skyeapi";
-import { getData } from "./functions";
-
-type QueryType = {
-    message: string,
-}
 
 const server = new SkyeAPI;
-server.name = "SkyeAPI";
-server.port = 8000;
 
-server.get("/data", getData);
-
-server.get("/data2", function (query: QueryType) {
-    return {
-        data: "data",
-        query: query.message,
-    }
-});
-
-server.post("/", function (body: any){
-    return body
+server.get("/", function() {
+    return "SkyeAPI"
 });
 
 server.start();
