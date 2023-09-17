25ddc2d0b793405004273822ca5d9fb8346db142
Herjus <herjus420@gmail.com>
2023-09-17 12:31:53 +0200

    ait

--- index.ts
+++ index.ts
@@ -9,17 +9,17 @@ const server = new SkyeAPI;
 server.name = "SkyeAPI";
 server.port = 8000;
 
-server.get("/data", getData)
+server.get("/data", getData);
 
 server.get("/data2", function (query: QueryType) {
     return {
         data: "data",
         query: query.message,
     }
-})
+});
 
 server.post("/", function (body: any){
     return body
-})
+});
 
-server.start()
+server.start();
