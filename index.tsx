1a8948e1abd459bc98e0b36b225b3ec17d5ad673
Herjus <herjus420@gmail.com>
2023-09-22 18:17:39 +0200

    okay

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import SkyeAPI from "./lib/skyeapi";
 
 const server = new SkyeAPI;
 
-server.get("/", function (query: any, params: any, res: any){
+server.get("/", function (query: any, params: any){
     return "Hello World"
 });
 
